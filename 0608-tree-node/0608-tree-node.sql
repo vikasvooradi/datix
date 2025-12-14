bebfb8debb38545df5dc54e36e06c5236d94ae94
Vikas Bhaskar Vooradi <vikas.b.vooradi@gmail.com>
2025-12-14 13:22:35 +0530

    Time: 503 ms (68.16%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+
+select 
+id , 
+CASE
+    when p_id is null then 'Root'
+    when id in (select p_id from tree) then 'Inner'
+    else 'Leaf' end as type 
+from tree;
