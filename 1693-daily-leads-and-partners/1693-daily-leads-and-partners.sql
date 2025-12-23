bad4380e85b791f3c612f06523301b88686ae3d0
Vikas Bhaskar Vooradi <vikas.b.vooradi@gmail.com>
2025-12-23 12:33:38 +0530

    Time: 652 ms (22.32%), Space: 0B (100%) - LeetHub

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+
+select 
+    to_char(date_id,'YYYY-MM-DD') as date_id,
+    make_name,
+    count(distinct lead_id) as unique_leads,
+    count(distinct partner_id)       as unique_partners
+from DailySales 
+    group by date_id,make_name order by date_id;
