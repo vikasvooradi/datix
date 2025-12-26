acf7319bdd8ac17885a57f4139fa9cae267b59ee
Vikas Bhaskar Vooradi <vikas.b.vooradi@gmail.com>
2025-12-26 15:51:40 +0530

    Time: 427 ms (91.33%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,8 +1,8 @@
 /* Write your PL/SQL query statement below */
 
 select 
-p.firstname as firstname,
-p.lastname as lastname,
-a.city as city,
-a.state as state 
-from person p left join address a on p.personid=a.personid
+    p.firstname as firstname,
+    p.lastname as lastname,
+    a.city as city,
+    a.state as state 
+from person p left join address a on p.personid=a.personid;
