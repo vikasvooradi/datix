5de35300dc4a1c1bf786e6fe6f959ed312b8dab1
Vikas Bhaskar Vooradi <vikas.b.vooradi@gmail.com>
2025-12-28 15:48:31 +0530

    Update 0175-combine-two-tables.sql

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,13 +1,15 @@
 /* Write your PL/SQL query statement below */
 
 SELECT
-    p.givenname AS givenname,
-    p.surname   AS surname,
-    a.cityname  AS cityname,
-    a.region    AS region
-FROM
-    customer p
-    LEFT JOIN location a ON p.customerid = a.customerid
-ORDER BY
+    p.givenname,
+    p.surname,
+    a.cityname,
+    a.region
+FROM customer p
+LEFT JOIN location a
+    ON p.customerid = a.customerid ORDER BY
     p.givenname;
+
+
+
 
