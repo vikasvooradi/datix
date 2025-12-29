885333059e7111e8a17c274d62454ee2b7dea243
Stackoverflow54 <stackoverflowquestion54@gmail.com>
2025-12-29 17:26:46 +0530

    Questions updated

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++
@@ -1,3 +0,0 @@
-/* Write your PL/SQL query statement below */
-
-select m.name as employee from employee m join employee e on m.managerid = e.id where m.salary > e.salary
---
+++ 181-staff-earning-more-than-supervisors/181-staff-earning-more-than-supervisors.sql
@@ -0,0 +1,5 @@
+/* Write your PL/SQL query statement below */
+
+select m.name as employee from employee m 
+join employee e on m.managerid = e.id 
+where m.salary > e.salary
