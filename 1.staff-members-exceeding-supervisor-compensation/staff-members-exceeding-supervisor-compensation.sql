2ed6d4bfdc5115caeb1e35cd95eb15d41a28a732
Vikas Bhaskar Vooradi <vikas.b.vooradi@gmail.com>
2025-12-31 15:37:58 +0530

    Update staff-members-exceeding-supervisor-compensation.sql

--- 1.staff-members-exceeding-supervisor-compensation/staff-members-exceeding-supervisor-compensation.sql
+++ 1.staff-members-exceeding-supervisor-compensation/staff-members-exceeding-supervisor-compensation.sql
@@ -1,1 +1,23 @@
-select * from dual;
+/* Write your PL/SQL query statement below */
+
+/*
+
+Explanation:
+
+p represents the staff member.
+s represents the supervisor of that staff member.
+We join the table to itself on supervisorCode = staffCode to link each employee with their supervisor.
+The WHERE clause filters only those staff whose payAmount is greater than their supervisor's payAmount.
+
+*/
+
+SELECT
+    p.fullname
+FROM
+         personnel p
+    JOIN personnel s ON p.supervisorcode = s.staffcode
+WHERE
+    p.payamount > s.payamount;
+
+
+
